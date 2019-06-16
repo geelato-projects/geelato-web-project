be788e58aa4a99a0562e8eed92a8be19c0fd3ea5
geelato <geelato@126.com>
2019-06-16 19:19:55 +0800

    init

--- src/main/java/org/geelato/web/project/ProjectApplication.java
+++ src/main/java/org/geelato/web/project/ProjectApplication.java
@@ -1,29 +1,18 @@
 package org.geelato.web.project;
 
 import org.geelato.web.platform.PlatformApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"org.geelato"})
+// 注意PlatformApplication中已有@SpringBootApplication，这里不需再写该注解，否则会执行两次run方法
 public class ProjectApplication extends PlatformApplication {
-    private static Logger logger = LoggerFactory.getLogger(ProjectApplication.class);
-
+//    private static Logger logger = LoggerFactory.getLogger(ProjectApplication.class);
 
     @Override
     public void run(String... strings) throws Exception {
         super.run(strings);
     }
 
-
     public static void main(String[] args) {
-//        if (args.length == 0) {
-//            args = new String[1];
-//            args[0] = "startFromMain";
-//        }
         SpringApplication.run(ProjectApplication.class, args);
     }
 }
